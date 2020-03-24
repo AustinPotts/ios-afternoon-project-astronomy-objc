2c1f5c376bead01c325064b6088034d074d06644
AustinPotts <pottsevolvements@gmail.com>
2020-03-24 11:31:50 -0400

    Configured Storyboard Layout + Objective C Subclass Files

---
+++ MarsRover-xcode/MarsRover-xcode/View Controllers/PEIPhotosViewController.h
@@ -0,0 +1,17 @@
+//
+//  PEIPhotosViewController.h
+//  MarsRover-xcode
+//
+//  Created by Austin Potts on 3/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEIPhotosViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MarsRover-xcode/MarsRover-xcode/View Controllers/PEIPhotosViewController.m
@@ -0,0 +1,35 @@
+//
+//  PEIPhotosViewController.m
+//  MarsRover-xcode
+//
+//  Created by Austin Potts on 3/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "PEIPhotosViewController.h"
+
+@interface PEIPhotosViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *roverImage;
+@property (weak, nonatomic) IBOutlet UILabel *roverDetailLabel;
+
+@end
+
+@implementation PEIPhotosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
